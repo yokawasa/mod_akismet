3d8274e98c20175017715bc1c1db03064cb6842e
Yoichi Kawasaki <yokawasa@gmail.com>
2009-11-29 10:52:59 +0900

    fix: modules.mk

--- modules.mk
+++ modules.mk
@@ -1,9 +1,13 @@
-MOD_AKISMET = mod_akismet akismet\
+MOD_AKISMET = mod_akismet akismet
 
-HEADER = akismet.h \
+HEADER = akismet.h
 
-mod_akismet.la: mod_akismet.slo
-	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version mod_akismet.lo
+${MOD_AKISMET:=.slo}: ${HEADER}
+${MOD_AKISMET:=.lo}: ${HEADER}
+${MOD_AKISMET:=.o}: ${HEADER}
+
+mod_akismet.la: ${MOD_AKISMET:=.slo}
+	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version ${MOD_AKISMET:=.lo}
 
 DISTCLEAN_TARGETS = modules.mk
 
